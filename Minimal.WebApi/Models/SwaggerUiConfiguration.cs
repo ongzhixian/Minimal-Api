fb9e6f659098173e51dd9e6b8e17e1ec1409671b
ongzhixian <zhixian@hotmail.com>
2025-09-13 00:33:12 +0800

    First model for SwaggerUi configuration

---
+++ Minimal.WebApi/Models/SwaggerUiConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace Expo.Models;
+
+public class SwaggerUiConfiguration
+{
+    public string DocumentTitle { get; set; } = string.Empty;
+    public string RoutePrefix { get; set; } = "/swagger/index.html";
+
+}
