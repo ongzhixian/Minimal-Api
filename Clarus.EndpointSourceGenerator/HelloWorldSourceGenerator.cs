a861129930073654c6022631d9e2836ee03460fa
ongzhixian <zhixian@hotmail.com>
2025-09-13 17:59:15 +0800

    Add source generation exampe

---
+++ Clarus.EndpointSourceGenerator/HelloWorldSourceGenerator.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace Clarus.EndpointSourceGenerator;
+
+[Generator]
+public class HelloWorldSourceGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        // This is where you would register your code generation logic.
+        // For a simple example, we'll just add a static source.
+        context.RegisterPostInitializationOutput(ctx =>
+        {
+            ctx.AddSource("HelloWorldGenerated.g.cs", @"
+namespace Clarus.EndpointSourceGenerator
+{
+    public static class HelloWorld
+    {
+        public static string SayHello()
+        {
+            return ""Hello, World! I was generated incrementally."";
+        }
+    }
+}");
+        });
+
+    } // END METHOD
+
+} // END CLASS
