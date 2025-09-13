fcf527b843a919358e02b2f7140525f2bd7fdf17
ongzhixian <zhixian@hotmail.com>
2025-09-13 10:43:33 +0800

    Add Serilog

--- Minimal.WebApi/Program.cs
+++ Minimal.WebApi/Program.cs
@@ -2,115 +2,138 @@ using Expo.Models;
 
 using Microsoft.OpenApi.Models;
 
-using Minimal.WebApi;
-
 using Scalar.AspNetCore;
 
-var builder = WebApplication.CreateBuilder(args);
+using Serilog;
 
-#region CONFIGURATIONS -- TO MOVE TO CONFIGURATIONS FOLDER AT A LATER DATE
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
 
-builder.Services.Configure<Microsoft.AspNetCore.Server.Kestrel.Core.KestrelServerOptions>(options =>
+try
 {
-    options.AddServerHeader = false;
-});
+    Log.Information("Starting web application");
 
-//builder.Services.AddCors(options =>
-//{
-//    const string MyAllowSpecificOrigins = "sdad";
-//    // Check if the application is running in a development environment.
-//    if (builder.Environment.IsDevelopment())
-//    {
-//        // For development, we can be more permissive to ease testing.
-//        // This policy allows any origin, method, and header.
-//        options.AddPolicy(name: MyAllowSpecificOrigins,
-//                          policy =>
-//                          {
-//                              policy.AllowAnyOrigin()
-//                                    .AllowAnyHeader()
-//                                    .AllowAnyMethod();
-//                          });
-//    }
-//    else // Production or other environments
-//    {
-//        // In production, we should only allow specific, trusted origins.
-//        // This makes your API more secure by preventing access from unwanted domains.
-//        // The allowed origins are read from the appsettings.json file.
-//        var allowedOrigins = builder.Configuration.GetSection("AllowedOrigins").Get<string[]>();
-//        if (allowedOrigins != null)
-//        {
-//            options.AddPolicy(name: MyAllowSpecificOrigins,
-//                              policy =>
-//                              {
-//                                  policy.WithOrigins(allowedOrigins)
-//                                        .AllowAnyHeader()
-//                                        .AllowAnyMethod();
-//                              });
-//        }
-//    }
-//});
+    var builder = WebApplication.CreateBuilder(args);
 
-var swaggerDocsConfiguration = new Dictionary<string, OpenApiInfo>();
-builder.Configuration.GetSection("SwaggerDocs").Bind(swaggerDocsConfiguration);
+    builder.Services.AddSerilog();
 
-#endregion
+    #region CONFIGURATIONS -- TO MOVE TO CONFIGURATIONS FOLDER AT A LATER DATE
 
-builder.Services.AddOpenApi(); // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
-{
-    Console.WriteLine("Numer of config found: {0}", swaggerDocsConfiguration.Count);
-    foreach (var config in swaggerDocsConfiguration) 
-        options.SwaggerDoc(config.Key, config.Value);
-});
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(options =>
+    builder.Services.Configure<Microsoft.AspNetCore.Server.Kestrel.Core.KestrelServerOptions>(options =>
     {
-        var swaggerUiConfiguration = new SwaggerUiConfiguration();
-        builder.Configuration.GetSection("SwaggerUi").Bind(swaggerUiConfiguration);
+        options.AddServerHeader = false;
+    });
 
-        options.DocumentTitle = swaggerUiConfiguration.DocumentTitle;
-        // Route prefix to access Swagger UI; default is "/swagger/index.html"
-        options.RoutePrefix = swaggerUiConfiguration.RoutePrefix;
+    //builder.Services.AddCors(options =>
+    //{
+    //    const string MyAllowSpecificOrigins = "sdad";
+    //    // Check if the application is running in a development environment.
+    //    if (builder.Environment.IsDevelopment())
+    //    {
+    //        // For development, we can be more permissive to ease testing.
+    //        // This policy allows any origin, method, and header.
+    //        options.AddPolicy(name: MyAllowSpecificOrigins,
+    //                          policy =>
+    //                          {
+    //                              policy.AllowAnyOrigin()
+    //                                    .AllowAnyHeader()
+    //                                    .AllowAnyMethod();
+    //                          });
+    //    }
+    //    else // Production or other environments
+    //    {
+    //        // In production, we should only allow specific, trusted origins.
+    //        // This makes your API more secure by preventing access from unwanted domains.
+    //        // The allowed origins are read from the appsettings.json file.
+    //        var allowedOrigins = builder.Configuration.GetSection("AllowedOrigins").Get<string[]>();
+    //        if (allowedOrigins != null)
+    //        {
+    //            options.AddPolicy(name: MyAllowSpecificOrigins,
+    //                              policy =>
+    //                              {
+    //                                  policy.WithOrigins(allowedOrigins)
+    //                                        .AllowAnyHeader()
+    //                                        .AllowAnyMethod();
+    //                              });
+    //        }
+    //    }
+    //});
 
+    var swaggerDocsConfiguration = new Dictionary<string, OpenApiInfo>();
+    builder.Configuration.GetSection("SwaggerDocs").Bind(swaggerDocsConfiguration);
+
+    #endregion
+
+    builder.Services.AddOpenApi(); // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen(options =>
+    {
+        Console.WriteLine("Numer of config found: {0}", swaggerDocsConfiguration.Count);
         foreach (var config in swaggerDocsConfiguration)
-            options.SwaggerEndpoint($"/{swaggerUiConfiguration.RoutePrefix}/{config.Key}/swagger.json", config.Key);
-
-        //options.SwaggerEndpoint("/swagger/01-EmptyApi/swagger.json", "01-EmptyApi");
-        //options.SwaggerEndpoint("/swagger/SampleApi/swagger.json", "SampleApi");
-        //options.SwaggerEndpoint("/swagger/MinimalExampleApi/swagger.json", "MinimalExampleApi");
-
-        //options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            options.SwaggerDoc(config.Key, config.Value);
     });
 
-    app.UseReDoc(options =>
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
     {
-        options.DocumentTitle = "Open API - ReDoc";
-        options.RoutePrefix = "api-docs"; // change relative path to the UI
-        options.SpecUrl("/openapi/v1.json");
-    });
+        app.UseSwagger();
+        app.UseSwaggerUI(options =>
+        {
+            var swaggerUiConfiguration = new SwaggerUiConfiguration();
+            builder.Configuration.GetSection("SwaggerUi").Bind(swaggerUiConfiguration);
 
-    app.MapScalarApiReference();
+            options.DocumentTitle = swaggerUiConfiguration.DocumentTitle;
+            // Route prefix to access Swagger UI; default is "/swagger/index.html"
+            options.RoutePrefix = swaggerUiConfiguration.RoutePrefix;
 
-    app.MapOpenApi();
+            foreach (var config in swaggerDocsConfiguration)
+                options.SwaggerEndpoint($"/{swaggerUiConfiguration.RoutePrefix}/{config.Key}/swagger.json", config.Key);
+
+            //options.SwaggerEndpoint("/swagger/01-EmptyApi/swagger.json", "01-EmptyApi");
+            //options.SwaggerEndpoint("/swagger/SampleApi/swagger.json", "SampleApi");
+            //options.SwaggerEndpoint("/swagger/MinimalExampleApi/swagger.json", "MinimalExampleApi");
+            //options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        });
+
+        app.UseReDoc(options =>
+        {
+            options.DocumentTitle = "Open API - ReDoc";
+            options.RoutePrefix = "api-docs"; // change relative path to the UI
+            options.SpecUrl("/openapi/v1.json");
+        });
+
+        app.MapScalarApiReference();
+
+        app.MapOpenApi();
+    }
+
+
+
+    app.UseHttpsRedirection();
+    //app.UseCors(MyAllowSpecificOrigins);
+    //app.UseAuthentication();
+    //app.UseAuthorization();
+
+    app.UseDefaultFiles();
+    app.UseStaticFiles();
+
+    //Minimal.WebApi.Routes.WeatherForecast.RegisterEndpoints(app);
+    //app.RegisterWeatherForecastApiEndpoints();
+
+
+    app.Run();
+
+
 }
-
-//Minimal.WebApi.Routes.WeatherForecast.RegisterEndpoints(app);
-app.RegisterWeatherForecastApiEndpoints();
-
-app.UseHttpsRedirection();
-//app.UseCors(MyAllowSpecificOrigins);
-//app.UseAuthentication();
-//app.UseAuthorization();
-
-app.UseDefaultFiles();
-app.UseStaticFiles();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
